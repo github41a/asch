6308c5efbcf761c3d863f3595f1c820f7d8a756e
github41a <134858618+github41a@users.noreply.github.com>
2023-05-30 13:10:32 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,257 +1,43 @@
-create release frequency report in anguler js and typescript  which have 2 option for  Report Type in drop down as Release Frequency distribution and Frequency distribution.  this page also have option to select date from dropdown from september 2021  to December- 2022. it also have submit button.  Can you use dummy data from from SQLITE: database and create dynamic pie chart based on that data. Use effectices tailwind.config.js
+<div class="w-full h-screen bg-slate-100 p-10">
+  <form class="p-4" (submit)="createChart()">
+    <label for="report-type">Report Type</label>
+    <select id="report-type" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-sm focus:ring-blue-500">
+      <option value="Release Frequency Distribution">Release Frequency Distribution</option>
+      <option value="Release Frequency Not Available">Release Frequency Not Available</option>
+    </select>
 
-Member
-Yes, I can help you create a release frequency report in AngularJS and TypeScript with the options you mentioned. Here is an example code snippet to get you started:
+    <label for="date">Select Date</label>
+    <select id="date" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-sm focus:ring-blue-500">
+      <option value="">-- Select Date --</option>
+      <option value="December 2022">December 2022</option>
+      <option value="November 2022">November 2022</option>
+      <option value="October 2022">October 2022</option>
+      <option value="September 2022">September 2022</option>
+      <!-- Add other date options as needed -->
+    </select>
 
-typescript
-Copy code
-import { Component } from '@angular/core';
-import { Chart } from 'chart.js';
-import sqlite3 from 'sqlite3';
+    <button class="bg-blue-500 px-2 py-1 text-white rounded-sm" type="submit">Submit</button>
+  </form>
 
-@Component({
-  selector: 'app-release-frequency-report',
-  templateUrl: './release-frequency-report.component.html',
-  styleUrls: ['./release-frequency-report.component.css']
-})
-export class ReleaseFrequencyReportComponent {
+  <canvas id="piechart" width="250" height="250"></canvas>
 
-  selectedReportType: string = 'Release Frequency distribution';
-  selectedDate: string = 'September 2021';
-  chart: Chart;
+  <div class="container">
+    <canvas id="myChart" width="250" height="250"></canvas>
+  </div>
 
-  constructor() {}
-
-  ngOnInit() {
-    this.generateChart();
-  }
-
-  generateChart() {
-    const db = new sqlite3.Database('path/to/database.sqlite');
-    const query = `SELECT COUNT(*) AS count FROM releases WHERE date >= '${this.selectedDate}' AND date <= 'December 2022'`;
-
-    db.all(query, (err, rows) => {
-      if (err) throw err;
-
-      const data = rows.map(row => row.count);
-      const labels = ['Q3 2021', 'Q4 2021', 'Q1 2022', 'Q2 2022', 'Q3 2022', 'Q4 2022'];
-
-      if (this.chart) {
-        this.chart.destroy();
-      }
-
-      this.chart = new Chart('canvas', {
-        type: 'pie',
-        data: {
-          labels,
-          datasets: [{
-            label: 'Release Frequency',
-            data,
-            backgroundColor: ['#ff6384', '#36a2eb', '#cc65fe', '#ffce56', '#4bc0c0', '#ff8f00']
-          }]
-        },
-        options: {
-          responsive: true,
-          maintainAspectRatio: false
-        }
-      });
-    });
-  }
-
-  onSubmit() {
-    this.generateChart();
-  }
-
-}
-This code assumes that you have already set up a database with a releases table that contains a date column. You can replace path/to/database.sqlite with the actual path to your database file.
-
-As for the Tailwind CSS configuration, you can customize it in your tailwind.config.js file. Here's an example:
-
-javascript
-Copy code
-module.exports = {
-  theme: {
-    extend: {}
-  },
-  variants: {},
-  plugins: []
-}
-You can add your own customizations to the extend object. For example, you can add custom colors, fonts, and spacing utilities.
-
-Member
-can you tell how to run this in codesandbox
-
-Member
-Sure! Here are the steps to run the AngularJS and TypeScript release frequency report in CodeSandbox:
-
-
-Go to codesandbox.io and create a new Angular project.
-
-In the terminal, install the following dependencies: 
-sqlite3
-, 
-knex
-, 
-chart.js
-, and 
-@types/chart.js
-.
-
-Create a new file 
-database.sqlite
- in the 
-public
- folder.
-
-Create a new file 
-knexfile.js
- in the root directory with the following code:
-
-javascript
-Copy code
-module.exports = {
-  client: 'sqlite3',
-  connection: {
-    filename: './public/database.sqlite',
-  },
-  useNullAsDefault: true,
-};
-
-Create a new file 
-db.js
- in the root directory with the following code:
-
-javascript
-Copy code
-const knex = require('knex');
-const db = knex({
-  client: 'sqlite3',
-  connection: {
-    filename: './public/database.sqlite',
-  },
-  useNullAsDefault: true,
-});
-module.exports = db;
-
-Create a new file 
-data.js
- in the root directory with the following code:
-
-javascript
-Copy code
-const db = require('./db');
-module.exports = {
-  async getReleaseFrequency() {
-    const data = await db
-      .select(db.raw('strftime(\'%m-%Y\', released_at) as month, count(*) as count'))
-      .from('releases')
-      .groupBy('month')
-      .orderBy('month');
-    return data.map(({ month, count }) => ({ month, count }));
-  },
-};
-
-Create a new file 
-app.js
- in the root directory with the following code:
-
-javascript
-Copy code
-const express = require('express');
-const app = express();
-const data = require('./data');
-app.get('/api/release-frequency', async (req, res) => {
-  const releaseFrequency = await data.getReleaseFrequency();
-  res.send(releaseFrequency);
-});
-app.listen(3000, () => console.log('Server listening on port 3000!'));
-
-Create a new file 
-app.component.ts
- in the 
-src/app
- directory with the following code:
-
-typescript
-Copy code
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import * as Chart from 'chart.js';
-interface ReleaseFrequency {
-  month: string;
-  count: number;
-} 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-})
-export class AppComponent implements OnInit {
-  title = 'Release Frequency Report';
-  releaseFrequency: ReleaseFrequency[] = [];
-  chart: Chart;
-  constructor(private http: HttpClient) {}
-  async ngOnInit() {
-    this.releaseFrequency = await this.http
-      .get<ReleaseFrequency[]>('/api/release-frequency')
-      .toPromise();
-    const data = {
-      labels: this.releaseFrequency.map((r) => r.month),
-      datasets: [
-        {
-          label: 'Number of Releases',
-          data: this.releaseFrequency.map((r) => r.count),
-          backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(255, 159, 64, 0.2)',
-          ],
-          borderColor: [
-            'rgba(255, 99, 132, 1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)',
-            'rgba(153, 102, 255, 1)',
-            'rgba(255, 159, 64, 1)',
-          ],
-          borderWidth: 1,
-        },
-      ],
-    };
-    const config = {
-      type: 'pie',
-      data,
-    };
-    this.chart = new Chart('myChart', config);
-  }
-}
-
-Create a new file 
-app.component.html
- in the 
-src/app
- directory with the following code:
-
-html
-Copy code
-<div class="container">
-  <h1>{{ title }}</h1>
-  <canvas id="myChart" width="400" height="400"></canvas>
+  <table>
+    <thead>
+      <tr>
+        <th>CIO</th>
+        <th>0-50</th>
+        <th>51-99</th>
+        <th>Equal to 100</th>
+        <th>Not Available</th>
+        <th>Total</th>
+      </tr>
+    </thead>
+    <tbody>
+      <!-- Add table data as needed -->
+    </tbody>
+  </table>
 </div>
-
-In the 
-index.html
- file, add the following code inside the 
-head
- tag to include the 
-chart.js
- library:
-
-html
-Copy code
-<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-
-Finally, customize the Tailwind CSS configuration in the 
